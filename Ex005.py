0b5ceefdbe82afd0234e4bfe86688d00fd593c19
ManoelCosta <47435630+ManoelCosta@users.noreply.github.com>
2019-07-25 19:28:11 -0300

    Este programa lê um número e mostra o sucessor e o antecessor dele.
    Lê se o numero do prompt através da função input(), o numero é convertido em inteiro através da função int() e o valor é guardado na variável num.
    A variavel suc contem o valor de num somado mais 1. A variavel ant contem o valor de num subtraído menos 1.
    Foram utilizados dentro do comando print() os seguintes comando: <\n> e <end=' '>
    <\n> quebra uma linha.
    <end=' '> imprime o que está na linha de baixo na mesma linha de cima.

---
+++ Ex005.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um valor: '))
+suc = num+1
+ant = num-1
+print('O número digitado foi {}\nO sucessor vale {}\nO antecessor vale {}.'.format(num, suc, ant), end=' ')
+print('Ta ok!')
