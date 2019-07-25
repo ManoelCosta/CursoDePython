c7e7ed280acdc35bc3b8aa21acb71fba76c50936
ManoelCosta <47435630+ManoelCosta@users.noreply.github.com>
2019-07-25 19:08:27 -0300

    Delete Ex005.py

--- Ex005.py
+++
@@ -1,5 +0,0 @@
-num = int(input('Digite um valor: '))
-suc = num+1
-ant = num-1
-print('O número digitado foi {}\nO sucessor vale {}\nO antecessor vale {}.'.format(num, suc, ant), end=' ')
-print('Ta ok!')
