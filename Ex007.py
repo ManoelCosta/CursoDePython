610b69fc521dcd6c196303bc6f66b1ee6f9804c7
Manoel Costa <ManoelCosta>
2019-07-26 18:08:10 -0300

    Este programa lê duas notas de um aluno, realiza os calculos e mostra a média do aluno A função input() é utilizada para pegar dados apartir do que se digita no teclado A função float() converte o que estiver dentro do parenteses para valores reais A função print() mostra algumas mensagem foi utilizado dentro da função print() o "\n", que realiza quebra de linha

---
+++ Ex007.py
@@ -0,0 +1,4 @@
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = (nota1 + nota2)/2
+print('Nota da primeira avaliação: {:.2f}\nNota da segunda avaliação: {:.2f}\nMedia do aluno: {:.2f}'.format(nota1, nota2, media))
