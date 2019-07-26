2948211a618bb269cee1ecebfd4fb826df89503e
Manoel Costa <ManoelCosta>
2019-07-26 18:14:11 -0300

    Este programa lê um valor em metro e mostra as conversões para dm, cm, mm, dam, hm, e km. Este programa usa a função: input() que pega dados do prompt que foram digitados no teclado float() que converte os valores dentro do parenteses para numeros reais print() que mostra alguma mensagem na tela dentro do print foi utilizado o "\n" que realiza uma quebra de linha

---
+++ Ex008.py
@@ -0,0 +1,20 @@
+m = float(input('Digite um valor em metro para conversão: '))
+dm = m*10
+cm = (m*10)*10
+mm = ((m*10)*10)*10
+dam= m/10
+hm = (m/10)/10
+km = ((m/10)/10)/10
+print('Valor digitado: {}m\n'.format(m))
+print('Conversão de metro para decímetro')
+print('{}m é igual a {}dm\n'.format(m, dm))
+print('Conversão de metro para centímetro')
+print('{}m é igual a {}cm\n'.format(m, cm))
+print('Conversão de metro para milímetro')
+print('{}m é igual a {}mm\n'.format(m, mm))
+print('Conversão de metro para decametro')
+print('{}m é igual a {}dam\n'.format(m, dam))
+print('Conversão de metro para hectometro')
+print('{}m é igual a {}hm\n'.format(m, hm))
+print('Conversão de metro para quilometro')
+print('{}m é igual a {}km'.format(m, km))
