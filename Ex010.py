8cc57d58b13f3a52ebb9164cef5948b7dd1f896a
Manoel Costa <ManoelCosta>
2019-07-31 18:44:01 -0300

    Programa le um valor e mostra na tela a quantidade de dolar que se pode comprar com ele. Este programa lê um valor do prompt através da função input(), converte esse valor para real através da função float(), armazena o resultado dentro da variavel carteira. Realiza o calculo de divisão entre a variavel carteira e o numero 3.27 e armazena o resultado dentro da variavel dolar. O programa mostra o resulatado na tela através da função print(). Dentro da função print() é utilizado as mascaras de saída '{}' e a função .format().

---
+++ Ex010.py
@@ -0,0 +1,3 @@
+carteira = float(input('Informe a quanto você tem de dinheiro: '))
+dolar = carteira / 3.27
+print('Seu saldo é: R${:.2f}\nA quantidade de dolares que você pode comprar é: U${:.2f}'.format(carteira, carteira))
